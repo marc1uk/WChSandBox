1004eeeac7d4af200f37d91afd53c3b411d974ab
Francesca Di Lodovico <f.di.lodovico@qmul.ac.uk>
2014-05-20 10:15:51 +0100

    amend the references

--- setupLite.sh
+++ setupLite.sh
@@ -3,8 +3,8 @@
 source ../hk-hyperk/Source_At_Start_T2HK.sh
 
 # for shared libraries
-export LD_LIBRARY_PATH=${parentdir}/annie-WCSimLite:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${parentdir}/hk-wchsandbox:$LD_LIBRARY_PATH
 
-export G4WORKDIR=${parentdir}/annie-WCSimLite
+export G4WORKDIR=${parentdir}/hk-wchsandbox
 
-export WCSIM=${parentdir}/annie-WCSimLite/src/
+export WCSIM=${parentdir}/hk-wchsandbox/src/
