64bcdc5968fcbee27122448064e94c04bcd61eb6
Matthew Wetstein <wetstein@lbnegpvm01.fnal.gov>
2014-12-04 00:14:46 -0600

    adding directory

---
+++ tmp/Linux-g++/WChSandBox/WCLitePrimaryGeneratorMessenger.d
@@ -0,0 +1,200 @@
+/lbne/app/users/wetstein/WChSandBox/tmp/Linux-g++/WChSandBox/WCLitePrimaryGeneratorMessenger.o /lbne/app/users/wetstein/WChSandBox/tmp/Linux-g++/WChSandBox/WCLitePrimaryGeneratorMessenger.d:  \
+ src/WCLitePrimaryGeneratorMessenger.cc \
+  include/WCLitePrimaryGeneratorMessenger.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UImessenger.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/globals.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ios.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4Types.hh \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/complex \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/c++config.h \
+  /usr/include/bits/wordsize.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/os_defines.h \
+  /usr/include/features.h /usr/include/sys/cdefs.h \
+  /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/cpu_defines.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/cpp_type_traits.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cmath \
+  /usr/include/math.h /usr/include/bits/huge_val.h \
+  /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+  /usr/include/bits/inf.h /usr/include/bits/nan.h \
+  /usr/include/bits/mathdef.h /usr/include/bits/mathcalls.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/cmath.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/sstream \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/istream \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/ios \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/iosfwd \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/c++locale.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cstring \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cstddef \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/include/stddef.h \
+  /usr/include/string.h /usr/include/xlocale.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cstdio \
+  /usr/include/stdio.h /usr/include/bits/types.h \
+  /usr/include/bits/typesizes.h /usr/include/libio.h \
+  /usr/include/_G_config.h /usr/include/wchar.h /usr/include/bits/wchar.h \
+  /usr/include/gconv.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/include/stdarg.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/clocale \
+  /usr/include/locale.h /usr/include/bits/locale.h \
+  /usr/include/langinfo.h /usr/include/nl_types.h /usr/include/iconv.h \
+  /usr/include/libintl.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/c++io.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/gthr.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/gthr-default.h \
+  /usr/include/pthread.h /usr/include/endian.h /usr/include/bits/endian.h \
+  /usr/include/sched.h /usr/include/time.h /usr/include/bits/sched.h \
+  /usr/include/bits/time.h /usr/include/signal.h \
+  /usr/include/bits/sigset.h /usr/include/bits/pthreadtypes.h \
+  /usr/include/bits/setjmp.h /usr/include/unistd.h \
+  /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+  /usr/include/bits/confname.h /usr/include/getopt.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cctype \
+  /usr/include/ctype.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stringfwd.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/postypes.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cwchar \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/ctime \
+  /usr/include/stdint.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/functexcept.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/exception_defines.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/exception \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/char_traits.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_algobase.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/climits \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/include/limits.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/include/syslimits.h \
+  /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+  /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+  /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cstdlib \
+  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+  /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+  /usr/include/sys/select.h /usr/include/bits/select.h \
+  /usr/include/sys/sysmacros.h /usr/include/alloca.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_pair.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_iterator_base_types.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_iterator_base_funcs.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/concept_check.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_iterator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/debug/debug.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/localefwd.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/ios_base.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/atomicity.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/atomic_word.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/locale_classes.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/string \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/memory \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/allocator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/c++allocator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/ext/new_allocator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/new \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_construct.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_uninitialized.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_raw_storage_iter.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/limits \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_function.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/basic_string.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/algorithm \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_algo.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_heap.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_tempbuf.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/basic_string.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/streambuf \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/streambuf.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/basic_ios.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/streambuf_iterator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/locale_facets.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/cwctype \
+  /usr/include/wctype.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/ctype_base.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/ctype_inline.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/codecvt.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/time_members.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/messages_members.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/basic_ios.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/istream.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/locale \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/locale_facets.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/typeinfo \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/ostream \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/ostream.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/sstream.tcc \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/iostream \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4String.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4String.icc \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/templates.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4PhysicalConstants.hh \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Units/PhysicalConstants.h \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Units/defs.h \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Units/SystemOfUnits.h \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4SystemOfUnits.hh \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Units/SystemOfUnits.h \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ExceptionSeverity.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ios.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/globals.hh \
+  include/WCLitePrimaryGeneratorAction.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/run/include/G4VUserPrimaryGeneratorAction.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ThreeVector.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/globals.hh \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Vector/ThreeVector.h \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Vector/defs.h \
+  /lbne/app/users/trj/wcsimsoft/CLHEP/include/CLHEP/Vector/ThreeVector.icc \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TTree.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TBranch.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TNamed.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TObject.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/Rtypes.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/RConfig.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/RVersion.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/DllImport.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/Rtypeinfo.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/snprintf.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/strlcpy.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TGenericClassInfo.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/vector \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_vector.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_bvector.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/vector.tcc \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TSchemaHelper.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TStorage.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TVersionCheck.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/Riosfwd.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TBuffer.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TString.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TMathBase.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TObjArray.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TSeqCollection.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TCollection.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TIterator.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/iterator \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stream_iterator.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TAttFill.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TDataType.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TDictionary.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/Property.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TAttLine.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TAttMarker.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TArrayD.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TArray.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TArrayI.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TClass.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TObjString.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/map \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_tree.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_map.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/stl_multimap.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TVirtualTreePlayer.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TRandom3.h \
+  /lbne/app/users/trj/wcsimsoft/ROOT/root/include/TRandom.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/fstream \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/x86_64-redhat-linux/bits/basic_file.h \
+  /usr/lib/gcc/x86_64-redhat-linux/4.1.2/../../../../include/c++/4.1.2/bits/fstream.tcc \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UIdirectory.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UIcommand.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UIparameter.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UItokenNum.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ApplicationState.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ThreeVector.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/intercoms/include/G4UIcmdWithAString.hh \
+  /lbne/app/users/trj/wcsimsoft/GEANT4/geant4.9.4.p02/source/global/management/include/G4ios.hh
