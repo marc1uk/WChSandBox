607978890bb404c1eb02c94e0a31ca5d3ad4cb40
Erika Catano Mur <ecatanom@iastate.edu>
2016-02-15 18:04:41 -0600

    Test a setup that doesn't point to a specific user's area nor assumes directory name

--- setupWChSandBox.sh
+++ setupWChSandBox.sh
@@ -5,14 +5,12 @@ source /grid/fermiapp/products/uboone/setup_uboone.sh
 setup root
 
 # setup G4parameters
-source /annie/app/users/wetstein/geant4.9.6.p04-build/share/Geant4-9.6.4/geant4make/geant4make.sh
-
+source /annie/app/externals/geant4.9.6.p04-build/share/Geant4-9.6.4/geant4make/geant4make.sh
+export G4BIN=/annie/app/externals/geant4.9.6.p04-build/bin/
 # for shared libraries
-export LD_LIBRARY_PATH=/annie/app/users/txin/WChSandBox:$LD_LIBRARY_PATH
-
-export G4WORKDIR=/annie/app/users/txin/WChSandBox
-
-export WCSIM=/annie/app/users/txin/WChSandBox/src/
+export G4WORKDIR=$PWD
+export LD_LIBRARY_PATH=$G4WORKDIR:$LD_LIBRARY_PATH
+export WCSIM=$G4WORKDIR/src
 
 export G4NEUTRONHP_USE_ONLY_PHOTONEVAPORATION=1
 
