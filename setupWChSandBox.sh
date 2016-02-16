d416d01901a681718a9aa7af0cf02f062e2aede9
Erika Catano Mur <ecatanom@iastate.edu>
2016-02-16 12:34:25 -0600

    Better setup

--- setupWChSandBox.sh
+++ setupWChSandBox.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# setup root
-source /grid/fermiapp/products/uboone/setup_uboone.sh
-setup root
-
-# setup G4parameters
+source /grid/fermiapp/products/common/etc/setup
+export PRODUCTS=${PRODUCTS}:/grid/fermiapp/products/uboone
+setup root 
 source /annie/app/externals/geant4.9.6.p04-build/share/Geant4-9.6.4/geant4make/geant4make.sh
-export G4BIN=/annie/app/externals/geant4.9.6.p04-build/bin/
-# for shared libraries
 export G4WORKDIR=$PWD
-export LD_LIBRARY_PATH=$G4WORKDIR:$LD_LIBRARY_PATH
-export WCSIM=$G4WORKDIR/src
-
+export LD_LIBRARY_PATH=${G4WORKDIR}:$LD_LIBRARY_PATH
 export G4NEUTRONHP_USE_ONLY_PHOTONEVAPORATION=1
+
+
+
 
